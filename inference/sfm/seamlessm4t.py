676c0c51e3b4ba87b16f8f12085c94a5f55a1eb7
Dominik Macháček <machacek@ufal.mff.cuni.cz>
2025-11-24 20:17:09 +0100

    seamless code cleaned

--- inference/sfm/seamlessm4t.py
+++ inference/sfm/seamlessm4t.py
@@ -23,19 +23,9 @@ def generate(model, sample):
     src = seamless_lang_codes[sample["src_lang"]]
     tgt = seamless_lang_codes[sample["tgt_lang"]]
 
-    task = "transcribe" if src == tgt else "translate"
-    is_long = True # TODO
-
-    # this is probably wrong, needs to be worked on next time
-    # docs: 
-    # https://huggingface.co/facebook/seamless-m4t-v2-large
-    # https://huggingface.co/docs/transformers/model_doc/seamless_m4t_v2 
-
     audio, sr = librosa.load(sample["sample"], sr=processor.feature_extractor.sampling_rate)
     inputs = processor(audios=audio, return_tensors="pt")
 
     out = model.generate(**inputs, tgt_lang=tgt, generate_speech=False)[0].cpu().numpy().squeeze()
-    print(out)
-
     text = processor.decode(out, skip_special_tokens=True)
     return text
